5215ae57fc1ebc8cbc1c22e5919720caccd6d337
SANNAYA-MUGHAL <102758335+SANNAYA-MUGHAL@users.noreply.github.com>
2023-11-17 20:26:25 +0500

    First assignment commit.

---
+++ index.js
@@ -0,0 +1,6 @@
+"use strict";
+console.log("good bye");
+var _name = "sana";
+console.log(_name);
+var myname = "sana liaqat";
+console.log(myname);
---
+++ index.ts
@@ -0,0 +1,46 @@
+console.log("good bye");
+
+var _name = "sana"
+console.log(_name);
+
+var myname = "sana liaqat";
+console.log(myname);
+
+var address= "pakistan";
+console.log(address);
+
+// comments regardind the code 
+
+var x = "sannaya";
+var y = "Mughal";
+var z = x + y;
+console.log(z);
+
+var x2 = 5.25;
+var y2 = 2.25;
+var z2 = x2 + y2;
+console.log(z2);
+
+var a = 1;
+var b = 2;
+var c = a + b;
+console.log(c);
+
+var x2 = 9.25;
+var y2 = 2.25;
+var z2 = x2 - y2;
+console.log(z2);
+
+var x2 = 5;
+var y2 = 2;
+var z2 = x2 * y2;
+console.log(z2);
+
+var x2 = 10;
+var y2 = 2;
+var z2 = x2 / y2;
+console.log(z2);
+
+
+
+
